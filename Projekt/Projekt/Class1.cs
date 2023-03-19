5f87b0265cb68624990725008f585e445e17aedd
umbc1ok <umbcio@gmail.com>
2023-03-19 21:39:40 +0100

    Added project files, and some code

---
+++ Projekt/Projekt/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Projekt
+{
+    public class Calculator
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+        public int subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public int square(int x) { return x * x; }
+
+    }
+}
