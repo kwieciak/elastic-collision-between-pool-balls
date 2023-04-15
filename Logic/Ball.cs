7ed566cc3d1bbffc20c08cdf6ff7d8cb521b3b6b
kwieciak <bkwiecinski02@gmail.com>
2023-04-15 17:34:48 +0200

    small changes

--- Logic/Ball.cs
+++ Logic/Ball.cs
@@ -3,56 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 
 
-// DOBRE PYTANIE CZY TA KLASA POWINNA BYĆ W LOGIC CZY DATA NA TEN MOMENT
 namespace Logic
 {
     internal class Ball
     {
-        //mozna sie zastanowic czy int czy float
-        private float posX;
-        private float posY;
-        private float radius;
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        public int Radius { get; set; }
 
-        private float speedX;
-        private float speedY;
+        public int SpeedX { get; set; }
+        public int SpeedY { get; set; }
 
-        
 
-        public Ball(float posX, float posY, float radius)   // nie wiem czy potrzebujemy tutaj od razu podawac predkosc
+
+        public Ball(int posX, int posY, int radius)   // nie wiem czy potrzebujemy tutaj od razu podawac predkosc
         {                                                   // czy jednak powinniśmy dopiero potem to robic
-            this.posX = posX;
-            this.posY = posY;
-            this.radius = radius;
+            this.PosX = posX;
+            this.PosY = posY;
+            this.Radius = radius;
         }
 
         public void moveBall()
         {
-            this.posX += speedX;
-            this.posY += speedY;
+            this.PosX += SpeedX;
+            this.PosY += SpeedY;
         }
-
-
-        // tutaj sa old-fashioned gettery i settery, sa zgrabniejsze i szybsze sposoby napisania tego
-        // ale nie wiem w sumie z ktorych jest lepiej korzystac
-        // https://codeeasy.io/lesson/properties
-        // wyzej masz link do tego jak mozna je zrobic i jak sie one zachowuja (SPOILER: zachowuja sie dziwnie)
-        // PS klasa Board ma ten system, mozesz sobie porownac
-
-        public float getPosX(){ return this.posX;}
-
-        public void setPosX(float posX){ this.posX=posX; }
-
-        public float getPosY() { return this.posY; }
-
-        public void setPosY(float posY){ this.posY=posY; }
-
-        public float getRadius() { return this.radius;}
-
-        //public void setRadius(float radius) { this.radius = radius;}      // IDK CZY TO POTRZEBNE
-
-        public float getSpeedX() { return this.speedX; }
-
-        public void setSpeedX(float speedX) {  this.speedX=speedX; }
 
     }
 }
--- Logic/LogicAPI.cs
+++ Logic/LogicAPI.cs
@@ -12,12 +12,12 @@ namespace Logic
         override public void MoveBall(){
 
         }
-        public override void AddBall(float x, float y, float radius){
+        public override void AddBall(int x, int y, int radius){
             Ball ball = new Ball(x,y,radius);
             // tutaj dodawanie kulki do Boarda
             // ALE: patrz LogicAbstractAPI (tam kwestionuje czy sie oplaca ta funkcje robic)
         }
-        public override void CreateBoard(int x, int y)
+        public override void CreateBoard(int x, int y, int ballAmount)
         {
             //throw new NotImplementedException();
         }
